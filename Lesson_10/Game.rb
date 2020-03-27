90725a4f5ef59e6af5e9636277e7a8c95f1629ec
Rutar Andriy <RutarAndriy@gmail.com>
2020-03-27 11:33:23 +0200

    Add Lesson 10

---
+++ Lesson_10/Game.rb
@@ -0,0 +1,39 @@
+# Дозволяємо використання методів із файлу <Methods.rb>
+
+require "./Methods.rb"
+
+# Отримуємо масив із загаданого слова
+
+mystery_word = get_Mystery_Word()
+
+# Визначаємо необхідні перемінніні
+
+errors_count = 0
+
+good_letters = []
+bad_letters = []
+
+# Основний цикл програми
+
+while errors_count < 7 do
+	
+	clear()
+
+	show_Status(mystery_word, good_letters, bad_letters, errors_count)
+
+	letter = get_User_Input()
+
+	result = check_Letter(letter, mystery_word, good_letters, bad_letters)
+
+	if (result == -1)
+		errors_count += 1
+	elsif (result == 1)
+		break
+	end
+
+end
+
+# Гра завершена - відображаємо результат
+
+clear
+show_Status(mystery_word, good_letters, bad_letters, errors_count)
