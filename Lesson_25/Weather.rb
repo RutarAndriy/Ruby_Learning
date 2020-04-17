5715d05db168ccf8f9320cf9de5de162f68cef9d
RutarAndriy <RutarAndriy@gmail.com>
2020-04-17 22:07:07 +0300

    Update Lesson 25

--- Lesson_25/Weather.rb
+++ Lesson_25/Weather.rb
@@ -3,24 +3,21 @@ require "net/http"
 require "nokogiri"
 
 # Адреса сайту, із якого беруться актуальні дані про погоду
+# Посилання використовує керуючі символи для правильного запису кирилиці
 
 url = "https://ua.sinoptik.ua/" +
 	  "%D0%BF%D0%BE%D0%B3%D0%BE%D0%B4%D0%B0-%D0%BB%D1%8C%D0%B2%D1%96%D0%B2"
 
 # Створюємо об'єкт типу <URI>
-
 uri = URI.parse(url)
 
 # Відправляємо запит і отримуємо результат
-
 response = Net::HTTP.get_response(uri)
 
 # Парсимо отриманий HTML
-
 doc = Nokogiri::HTML(response.body)
 
 # Виділяємо блок, який містить необхідну нам інформацію
-
 main_block = doc.css('.main')[0]
 
 # Виділяємо необхідні нам дані
